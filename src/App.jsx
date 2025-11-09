66ea491f85fc4058702ecd40a4b33aca4cc9a5f9
Bee345 <lilbeezee8@gmail.com>
2025-11-10 00:50:08 +0100

    Am About to start another part of header work here

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@
 import React from 'react'
 import './App.css'
 import './index.css'
+import LHeader from './components/LHeader';
 
 function App() {
   // const [count, setCount] = useState(0);
@@ -31,12 +32,17 @@ function App() {
         Click on the Vite and React logos to learn more
       </p> */}
 
+     <LHeader/>
       <section>
-        <h1 className='bg-amber-600 text-center font-bold'>Welcome, I hope this one won't be a problem to face</h1>
-        <div className="displayBox bg-gradient-to-t from-[#fd4] to-[#d5b] w-[80%] h-[200px] mx-auto my-8 flex justify-center gap-20 text-center items-center flex-wrap rounded-2xl"><p>We re good on out own but w e need to make it cool off </p> <p>Sensing the change in the energy will make u go better to the adaptability</p></div>
+        <h1 className='bg-amber-600 text-center font-bold'>Welcome, I hope this one won't be a problem to face</h1> 
+
+        <div className="displayBox bg-gradient-to-t from-[#fd4] via-[#3d4] to-[#d5b] w-[80%] h-[200px] mx-auto my-8 flex justify-center gap-20 text-center items-center flex-wrap rounded-2xl"><p>We re good on out own but w e need to make it cool off </p> <p>Sensing the change in the energy will make u go better to the adaptability</p></div>
       </section>
     </>
   )
 }
 
-export default App
+
+export default App;
+
+
---
+++ src/components/LSHeader.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LSHeader = () => {
+  return (
+   <> 
+   
+   </>
+  )
+}
+
+export default LSHeader
